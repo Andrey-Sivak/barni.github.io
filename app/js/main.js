e3a09886a39e28d3a007ff9275ddcd03e80766ed
a.sivak <riziy_bes@mail.ru>
2021-08-30 16:37:03 +0300

    init

---
+++ app/js/main.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const mobileWidth = 767;
+let isMobile = checkWidth();
+
+window.addEventListener('resize', () => {
+    isMobile = checkWidth();
+});
+
+setTimeout(() => {
+    if (!document.querySelector('.loader')) {
+        return;
+    }
+
+    const loader = document.querySelector('.loader');
+    if (loader.classList.contains('active')) {
+        loader.classList.remove('active');
+
+        setTimeout(() => {
+            loader.parentElement.removeChild(loader);
+        }, 300)
+    }
+}, 2500);
+
+window.addEventListener('load', function () {
+
+    (function loader() {
+        if (!document.querySelector('.loader')) {
+            return;
+        }
+
+        const loader = document.querySelector('.loader');
+
+        if (loader.classList.contains('active')) {
+            loader.classList.remove('active');
+        }
+
+        setTimeout(() => {
+            loader.parentElement.removeChild(loader);
+        }, 1500);
+
+    })();
+
+    (function mobMenu() {
+        if (!document.querySelector('.mob-menu-btn')) {
+            return;
+        }
+
+        const btn = document.querySelector('.mob-menu-btn');
+        const menu = document.querySelector('.menu');
+
+        btn.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            this.classList.toggle('active');
+            menu.classList.toggle('active');
+            document.body.classList.toggle('no-scrolling');
+        });
+
+        if (btn.classList.contains('hide')) {
+            btn.classList.remove('hide');
+        }
+    })();
+
+});
+
+function checkWidth() {
+    return mobileWidth > document.documentElement.clientWidth;
+}
